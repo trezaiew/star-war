9d4f19897b9c37a62578f8fa4ecb1ffd751fc143
trezaiew <taherehrezai95@gmail.com>
2023-03-28 22:05:44 +0430

    clean code

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -1,15 +1,15 @@
-const EnHancedFetch = (method,url,data)=> {
+const EnHancedFetch = (method, url, data) => {
     return fetch(url)
-    .then(res => {
-        if (!res.ok) {
-            throw new Error(res.status); 
-        }
-      
-        return res.json();
-    })
-    .catch(error => {
-        console.log(error);
-    })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            return res.json();
+        })
+        .catch(error => {
+            console.log(error);
+        })
 }
 
 export default EnHancedFetch;
