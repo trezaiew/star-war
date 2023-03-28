988121e8001cdb1d213938f9bbc074eeb627095c
trezaiew <taherehrezai95@gmail.com>
2023-03-28 22:04:59 +0430

    clean code

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-const Pagination = ({ postsPerPage, totalPosts, paginate,currentPage,setCurrentPage,Posts,nextPage,previousPage }) => {
-  const PageNumbers = [];
-   
-  const int = Math.ceil(totalPosts / postsPerPage)
-    if (int === 1 ) 
-        return null 
-     for (let i = 1; i<= int; i++) {
-        PageNumbers.push(i) 
+const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage, setCurrentPage, Posts, nextPage, previousPage }) => {
+    const PageNumbers = [];
+
+    const int = Math.ceil(totalPosts / postsPerPage)
+    if (int === 1)
+        return null
+    for (let i = 1; i <= int; i++) {
+        PageNumbers.push(i)
     }
-    
-   
+
+
     return (
         <nav>
-          <h4 className='d-flex flex-column align-items-center justify-content-center '>{currentPage}</h4>
+            <h4 className='d-flex flex-column align-items-center justify-content-center '>{currentPage}</h4>
             <ul className="pagination">
                 <li className="page-item " >
                     <a onClick={() => previousPage()} href="!#"
-                    className={currentPage === 1 ? "page-link disabled" : "page-link"}
-                    disabled={currentPage === 1}>
+                        className={currentPage === 1 ? "page-link disabled" : "page-link"}
+                        disabled={currentPage === 1}>
                         Previous
                     </a>
                 </li>
@@ -31,14 +31,14 @@ const Pagination = ({ postsPerPage, totalPosts, paginate,currentPage,setCurrentP
                 ))}
                 <li className='page-item'>
                     <a onClick={() => nextPage()} href="!#"
-                      className={currentPage === PageNumbers.length ? "page-link disabled" : "page-link"}
-                      disabled={currentPage === PageNumbers.length}>
+                        className={currentPage === PageNumbers.length ? "page-link disabled" : "page-link"}
+                        disabled={currentPage === PageNumbers.length}>
                         Next
                     </a>
                 </li>
             </ul>
         </nav>
-  )
+    )
 };
 
 export default Pagination;
