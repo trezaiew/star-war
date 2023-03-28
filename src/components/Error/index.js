313499a9635554b6fa6e126fbe447bd8ce3839f0
trezaiew <taherehrezai95@gmail.com>
2023-03-28 22:04:32 +0430

    clean code

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -4,14 +4,14 @@ const Error = () => {
     const HandleError = () => {
         window.location.href = "/";
 
-    return (
-        <div className="alert alert-danger" role="alert">
-            <h4 className="alert-heading">Sorry! You got an error!</h4>
-           
-            <button className="btn" onClick={HandleError}>Try Again</button>
-        </div>
-    )
-}
+        return (
+            <div className="alert alert-danger" role="alert">
+                <h4 className="alert-heading">Sorry! You got an error!</h4>
+
+                <button className="btn" onClick={HandleError}>Try Again</button>
+            </div>
+        )
+    }
 }
 
 export default Error
