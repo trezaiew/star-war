9a9d4c8e3a6e154f44b912835567f3547914724b
trezaiew <taherehrezai95@gmail.com>
2023-03-28 22:04:39 +0430

    clean code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,31 @@
 import './styles.css';
 
-function Header(){
-    return(
-    
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a className="navbar-brand" href="#">Star Wars</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Movies & TV</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">About Us</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    )
+function Header() {
+  return (
+
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">Star Wars</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Movies & TV</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About Us</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Header
